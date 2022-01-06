7cc6ab9546160014e22273fbb80893d973557091
Tadas <tadas1985@gmail.com>
2022-01-06 19:23:03 +0200

    Tasks 1 and 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task 1
+            int r = 6371;
+
+            var area = 4 * (22 / 7) * r * r;
+
+            // Task 2
+           var volume = (float)(4.0 / 3 * Math.PI * r * r * r);
         }
     }
 }
